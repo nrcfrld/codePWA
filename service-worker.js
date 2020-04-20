106c241602384e756509f7473645747f00fffa52
Enrico Feraldo Kalengkongang <50624614+nrcfrld@users.noreply.github.com>
2020-04-20 07:09:13 +0700

    Add files via upload
    First Commit

---
+++ service-worker.js
@@ -0,0 +1,63 @@
+var dataCacheName = 'spa-data-v1';
+var cacheName = 'spa-cache-v1';
+var filesToCache = [
+ "./",
+ "./favicon.ico",
+ "./images",
+ "./images/icons",
+ "./images/icons/icon-128x128.png",
+ "./images/icons/icon-144x144.png",
+ "./images/icons/icon-152x152.png",
+ "./images/icons/icon-192x192.png",
+ "./images/icons/icon-256x256.png",
+ "./images/icons/icon-512x512.png",
+ "./index.html",
+ "./page1.html",
+ "./page2.html",
+ "./page3.html",
+ "./manifest.json",
+ "./js",
+ "./js/app.js",
+ "./js/jquery.min.js",
+ "./js/materialize.min.js",
+ "./js/routepage.js",
+ "./service-worker.js",
+ "./styles",
+ "./styles/style.css",
+ "./styles/materialize.min.css"
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName && key !== dataCacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[Service Worker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
+
